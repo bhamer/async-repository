042b43d28b8011d19eca58b61eb4383124be7f7e
Brandon <hamerb10@gmail.com>
2017-02-28 18:39:09 -0800

    Update IUnitOfWork.cs

--- AsyncRepository/UnitsOfWork/IUnitOfWork.cs
+++ AsyncRepository/UnitsOfWork/IUnitOfWork.cs
@@ -5,7 +5,29 @@ namespace AsyncRepository.UnitsOfWork
 {
     public interface IUnitOfWork : IDisposable
     {
-        void Commit();
-        Task CommitAsync();
+        /// <summary>
+        /// Sends all changes to the data store and commits as a single transaction unless a manual transaction was created using BeginTransaction().
+        /// </summary>
+        void Commit(string changeUser);
+        
+        /// <summary>
+        /// Asynchronously sends all changes to the data store and commits as a single transaction unless a manual transaction was created using BeginTransaction().
+        /// </summary>
+        Task CommitAsync(string changeUser);
+
+        /// <summary>
+        /// Starts a transaction. This can be used to call Commit() multiple times and treat it as a single transaction.
+        /// </summary>
+        IDisposable BeginTransaction();
+
+        /// <summary>
+        /// Commits the transaction that was started using BeginTransaction().
+        /// </summary>
+        void CommitTransaction();
+
+        /// <summary>
+        /// Rolls back the transaction that was started using BeginTransaction().
+        /// </summary>
+        void RollbackTransaction();
     }
 }
